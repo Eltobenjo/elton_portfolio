3de195a7c8e6f1b9d89d441832ab9bb32323ea28
Elton Benjamin <eltobenjo@gmail.com>
2021-02-22 19:27:40 +0100

    add Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import Card from "@material-ui/core/Card";
-
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -9,6 +7,27 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import html from "./images/html.png";
+import css from './images/css.png'
+import js from './images/js.png'
+import react from './images/react.png'
+import node from './images/node.png'
+import sass from './images/sass.png'
+import materialui from './images/materialui.png'
+import trello from './images/trello.png'
+import scrum from './images/scrum.png'
+import mongodb from './images/mongodb.png'
+import netlify from './images/netlify.png'
+import heroku from './images/heroku.png'
+import bulma from './images/bulma.png'
+import git from './images/git.png'
+import github from './images/github.png'
+import sql from './images/sql.png'
+import mysql from './images/mysql.png'
+import postgresql from './images/postgresql.png'
+import vscode from './images/vscode.png'
+import bootstrap from "./images/bootstrap.png";
+import jira from './images/jira.png'
 
 
 
@@ -31,10 +50,14 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
     display: "flex",
     flexDirection: "column",
+    
   },
   cardMedia: {
     paddingTop: "56.25%", // 16:9
+    
   },
+
+
   cardContent: {
     flexGrow: 1,
   },
@@ -42,9 +65,143 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
+
 }));
 
-const cards = [1, 2, 3, 4,5, 6,7,8,9];
+const cards = [
+  {
+    id: "1",
+    name: "HTML 5",
+    img: html,
+    Description: "",
+  },
+  {
+    id: "2",
+    name: "CSS 3",
+    img: css,
+    Description: " ",
+  },
+  {
+    id: "3",
+    name: "Sass",
+    img: sass,
+    Description: " ",
+  },
+  {
+    id: "4",
+    name: "Bulma",
+    img: bulma,
+    Description: "",
+  },
+
+  {
+    id: "5",
+    name: "Bootstrap",
+    img: bootstrap,
+    Description: "",
+  },
+  {
+    id: "6",
+    name: "Material UI",
+    img: materialui,
+    Description: "",
+  },
+
+  {
+    id: "7",
+    name: "Javascript",
+    img: js,
+    Description: " ",
+  },
+  {
+    id: "8",
+    name: "React",
+    img: react,
+    Description: "",
+  },
+  {
+    id: "9",
+    name: "Node & Express JS",
+    img: node,
+    Description: "",
+  },
+
+  {
+    id: "10",
+    name: "MongoDB",
+    img: mongodb,
+    Description: "",
+  },
+
+  {
+    id: "11",
+    name: "PostgreSQL",
+    img: postgresql,
+    Description: "",
+  },
+  {
+    id: "12",
+    name: "SQL",
+    img: sql,
+    Description: "",
+  },
+  {
+    id: "13",
+    name: "MySQL",
+    img: mysql,
+    Description: "",
+  },
+
+  {
+    id: "14",
+    name: "VS-Code",
+    img: vscode,
+    Description: "",
+  },
+  {
+    id: "15",
+    name: "Heroku",
+    img: heroku,
+    Description: "",
+  },
+  {
+    id: "16",
+    name: "Netlify",
+    img: netlify,
+    Description: "",
+  },
+  {
+    id: "17",
+    name: "Git Hub",
+    img: github,
+    Description: "",
+  },
+  {
+    id: "18",
+    name: "Git",
+    img: git,
+    Description: "",
+  },
+  {
+    id: "19",
+    name: "Agile & Scrum",
+    img: scrum,
+    Description: "",
+  },
+  {
+    id: "20",
+    name: "Trello",
+    img: trello,
+    Description: "",
+  },
+  {
+    id: "21",
+    name: "Jira",
+    img: jira,
+    Description: "",
+  },
+];
+;
 
 export default function Skills() {
   const classes = useStyles();
@@ -69,11 +226,8 @@ export default function Skills() {
               variant="h5"
               align="center"
               color="textSecondary"
-              paragraph
-            >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
+              paragraph>
+             
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
@@ -87,22 +241,19 @@ export default function Skills() {
           {/* End hero unit */}
           <Grid container spacing={4}>
             {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+              
+              <Grid item key={card.id} xs={6} sm={12} md={4}>
+                
                 <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
+                  <CardMedia className={classes.cardMedia} image={card.img} />
                   <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      My first project
-                    </Typography>
-                    <Typography>
-                      Built this with basic HTML and CSS knowledge.
-                    </Typography>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                    >{card.name}</Typography>
+                    <Typography></Typography>
                   </CardContent>
-                  
                 </Card>
               </Grid>
             ))}
